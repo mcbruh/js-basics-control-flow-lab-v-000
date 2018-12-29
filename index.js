5c5141bcbc23ed1d33d98a1499b2463c635632ae
Michael Chen <michael.chen.1120@gmail.com>
2018-12-29 01:02:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function scuberGreetingForFeet(value) {
 
 function ternaryCheckCity(city) {
   city = "NYC" ? (return "OK, sounds good.") : (return "No go.")
-  
 }
 
 function switchOnCharmFromTip() {

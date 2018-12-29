b4b43f39e1b813f70bac35af1e9502d22fe647d0
Michael Chen <michael.chen.1120@gmail.com>
2018-12-29 00:50:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ function scuberGreetingForFeet(value) {
   if (value <= 400) {
     return "This one is on me!"
   }
-  else if (value > 2000) {
+  else if (value > 2000 && value < 2500) {
     return "I will gladly take your thirty bucks."
+  }
+  else {
+    return "No can do."
   }
 };
 
